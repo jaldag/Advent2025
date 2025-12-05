45172eaa6849d8e969ccf4a6f7304652265a7bfc
James Aldag <jamesaldag@gmail.com>
2025-12-05 11:51:43 -0800

    D5

--- D5P1.py
+++ D5P1.py
@@ -1,26 +1,11 @@
-fresh = set()
+fresh = 0
 
 with open("D5Input.txt") as file:
 	ranges, ids = [item.splitlines() for item in file.read().strip().split("\n\n")]
 
-numRanges = []
-
-for line in ranges:
-	numRanges.append([int(line.split("-")[0]), int(line.split("-")[1])])
-
-numRanges.sort()
-
-compress = [numRanges.pop(0)]
-
-for start, end in numRanges:
-	if start > compress[-1][1]:
-		compress.append([start, end])
-	else:
-		compress[-1][1] = max(end, compress[-1][1])
-
-total = 0
-
-for start, end in compress:
-	total += end - start + 1
-
-print(total)
+for num in ids:
+	for line in ranges:
+		if int(num) >= int(line.split("-")[0]) and int(num) <= int(line.split("-")[1]):
+			fresh += 1
+			break
+print(fresh)
