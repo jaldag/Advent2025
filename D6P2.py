bc0809e111174e39b12e2f06c468fced9cd278f4
James Aldag <jamesaldag@gmail.com>
2025-12-06 00:44:16 -0800

    D6

--- D6P2.py
+++ D6P2.py
@@ -9,26 +9,29 @@ total = 0
 nums = []
 
 #Each line is the same length
-#Iterate through the line's length backwards
-for i in range(len(rows[0])-1, -1, -1):
-	num = ""
+#Iterate through the line's length
+for i in range(0, len(rows[0])):
+	#Save the operation if it's in this column
+	if rows[-1][i] != ' ':
+		operation = rows[-1][i]
+	
 	#Build the number from the column, ignoring last operational row
+	num = ""
 	for j in range(0, len(rows) - 1):
 		if rows[j][i].isdigit():
 			num += rows[j][i]
-	#Skip column if empty column
-	if num == "":
-		continue
-	nums.append(int(num))
-
-	#If last row at position 'i' is an operation, it's time to math the numbers together
-	#After, reset the numbers to be math'd
-	if rows[-1][i] != ' ':
-		operation = rows[-1][i]
-		if operation == "+":
-			total += sum(nums)
-		else:
-			total += math.prod(nums)
-		nums = []
+	#If the column built a number, save it
+	if num.isdigit():
+		nums.append(int(num))
+		#Move on to next column, unless it's the last column
+		if i != len(rows[0])-1:
+			continue
+	#If we're here, it means we're in the last column or in an empty column
+	#Time for some quick math
+	if operation == "+":
+		total += sum(nums)
+	else:
+		total += math.prod(nums)
+	nums = []
 
 print(total)
